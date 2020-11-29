7b5d631e9dc9e0e5ab6071189935b57b569f2ce3
Luo Meng <53254812+luomeng007@users.noreply.github.com>
2020-11-29 16:39:22 +0100

    Add files via upload

---
+++ getDesktopWindow.py
@@ -0,0 +1,24 @@
+# -*- coding:utf-8 -*-
+"""
+author: 15025
+age: 26
+e-mail: 1502506285@qq.com
+time: 2020/11/29 12:47
+software: PyCharm
+
+Description:
+"""
+import win32gui
+
+
+class Tutorial:
+    @staticmethod
+    def mainDebug():
+        handle = win32gui.GetDesktopWindow()
+        # we could use this handle to deal with desktop window
+        print(handle)   # 65552
+
+
+if __name__ == '__main__':
+    main = Tutorial()
+    main.mainDebug()
