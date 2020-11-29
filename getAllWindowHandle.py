0e887881a55b4593555c83fd3cd4082ee95eb0b0
Luo Meng <53254812+luomeng007@users.noreply.github.com>
2020-11-29 17:50:39 +0100

    Add files via upload

---
+++ getAllWindowHandle.py
@@ -0,0 +1,27 @@
+# -*- coding:utf-8 -*-
+"""
+author: 15025
+age: 26
+e-mail: 1502506285@qq.com
+time: 2020/11/29 17:11
+software: PyCharm
+
+Description:
+"""
+import win32gui
+
+
+class Win32:
+    @staticmethod
+    def tutorial():
+        hWndList = []
+        win32gui.EnumWindows(lambda hWnd, param: param.append(hWnd), hWndList)
+        print(hWndList)
+        for hwnd in hWndList:
+            title = win32gui.GetWindowText(hwnd)
+            print(title)
+
+
+if __name__ == '__main__':
+    main = Win32()
+    main.tutorial()
